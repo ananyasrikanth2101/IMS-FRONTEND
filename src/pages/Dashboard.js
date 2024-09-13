aaffa92789047e2015e3753f2a6715701a96eea0
ananyasrikanth2101 <ananyasrikanth03@gmail.com>
2024-09-13 18:37:59 +0530

    commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -104,28 +104,25 @@ function Dashboard() {
       .catch((err) => console.log(err));
   }, [authContext.user]);
 
-  // Memoized updateChartData function
-  const updateChartData = useCallback(
-    (salesData) => {
-      setChart((prevChart) => ({
-        ...prevChart,
-        series: [
-          {
-            name: "Monthly Sales Amount",
-            data: [...salesData],
-          },
-        ],
-      }));
-    },
-    [setChart]
-  );
-
   const fetchMonthlySalesData = useCallback(() => {
     fetch(`https://ims-backend-3.onrender.com/api/sales/getmonthly`)
       .then((response) => response.json())
       .then((datas) => updateChartData(datas.salesAmount))
       .catch((err) => console.log(err));
-  }, [updateChartData]);
+  }, [authContext.user]);
+
+  // Update Chart Data
+  const updateChartData = (salesData) => {
+    setChart({
+      ...chart,
+      series: [
+        {
+          name: "Monthly Sales Amount",
+          data: [...salesData],
+        },
+      ],
+    });
+  };
 
   useEffect(() => {
     fetchTotalSaleAmount();
@@ -144,7 +141,6 @@ function Dashboard() {
   return (
     <>
       <div className="grid grid-cols-1 col-span-12 lg:col-span-10 gap-6 md:grid-cols-3 lg:grid-cols-4  p-4 ">
-        {/* Sale and Purchase Cards */}
         <article className="flex flex-col gap-4 rounded-lg border  border-gray-100 bg-white p-6  ">
           <div className="inline-flex gap-2 self-end rounded bg-green-100 p-1 text-green-600">
             <svg
@@ -161,22 +157,126 @@ function Dashboard() {
                 d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
               />
             </svg>
+
             <span className="text-xs font-medium"> 67.81% </span>
           </div>
+
           <div>
             <strong className="block text-sm font-medium text-gray-500">
               Sales
             </strong>
+
             <p>
               <span className="text-2xl font-medium text-gray-900">
                 ${saleAmount}
               </span>
+
               <span className="text-xs text-gray-500"> from $240.94 </span>
             </p>
           </div>
         </article>
-        {/* Other Cards */}
-        {/* Chart Section */}
+
+        <article className="flex flex-col  gap-4 rounded-lg border border-gray-100 bg-white p-6 ">
+          <div className="inline-flex gap-2 self-end rounded bg-red-100 p-1 text-red-600">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6"
+              />
+            </svg>
+
+            <span className="text-xs font-medium"> 67.81% </span>
+          </div>
+
+          <div>
+            <strong className="block text-sm font-medium text-gray-500">
+              Purchase
+            </strong>
+
+            <p>
+              <span className="text-2xl font-medium text-gray-900">
+                {" "}
+                ${purchaseAmount}{" "}
+              </span>
+
+              <span className="text-xs text-gray-500"> from $404.32 </span>
+            </p>
+          </div>
+        </article>
+        <article className="flex flex-col   gap-4 rounded-lg border border-gray-100 bg-white p-6 ">
+          <div className="inline-flex gap-2 self-end rounded bg-red-100 p-1 text-red-600">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6"
+              />
+            </svg>
+
+            <span className="text-xs font-medium"> 67.81% </span>
+          </div>
+
+          <div>
+            <strong className="block text-sm font-medium text-gray-500">
+              Total Products
+            </strong>
+
+            <p>
+              <span className="text-2xl font-medium text-gray-900">
+                {" "}
+                {products.length}{" "}
+              </span>
+            </p>
+          </div>
+        </article>
+        <article className="flex flex-col   gap-4 rounded-lg border border-gray-100 bg-white p-6 ">
+          <div className="inline-flex gap-2 self-end rounded bg-red-100 p-1 text-red-600">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6"
+              />
+            </svg>
+
+            <span className="text-xs font-medium"> 67.81% </span>
+          </div>
+
+          <div>
+            <strong className="block text-sm font-medium text-gray-500">
+              Total Stores
+            </strong>
+
+            <p>
+              <span className="text-2xl font-medium text-gray-900">
+                {" "}
+                {stores.length}{" "}
+              </span>
+            </p>
+          </div>
+        </article>
         <div className="flex justify-around bg-white rounded-lg py-8 col-span-full justify-center">
           <div>
             <Chart
